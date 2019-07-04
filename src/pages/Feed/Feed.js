f287fd51bf524a7fc47caed2ea3279cc120f72fe
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-07-04 22:12:39 +0200

    Sending the create post query

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -133,35 +133,43 @@ class Feed extends Component {
     formData.append('content', postData.content);
     formData.append('image', postData.image);
 
-    let url = 'http://localhost:8080/feed/post';
-    let method = 'POST';
-    if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
-      method = 'PUT';
+    let graphqlQuery = {
+      query: `
+        mutation {
+            createPost(postInput: {title: "${postData.title}", content: "${postData.content}", imageUrl: "some url"}) {
+              _id
+              title
+              content
+              imageUrl
+              creator {
+                name
+              }
+              createdAt
+            }
+        }
+      `
     }
 
-    fetch(url, {
-      method,
-      // if you want to upload img you cannot use json
-      // headers: {
-      //   'Content-Type': 'application/json'
-      // },
-      // body: JSON.stringify({
-      //     title: postData.title,
-      //     content: postData.content
-      // })
-        body: formData,
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
+        body: JSON.stringify(graphqlQuery),
         headers: {
-            Authorization: 'Bearer ' + this.props.token
+            Authorization: 'Bearer ' + this.props.token,
+            'Content-Type': 'application/json'
         }
     })
       .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Creating or editing a post failed!');
-        }
         return res.json();
       })
       .then(resData => {
+          if (resData.errors && resData.error[0].status === 422) {
+              throw new Error(
+                  "Validation failed. Make sure the email address isn't used yet!"
+              );
+          }
+          if (resData.errors) {
+              throw new Error('User LOGIN failed!');
+          }
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
