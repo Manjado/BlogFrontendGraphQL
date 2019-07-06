49b8426a24f711bf62b224ac1669c330edb0a8a3
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-07-06 12:44:57 +0200

    loading posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -54,26 +54,45 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page,{
+    const grapqlQuery = {
+        query: `{
+         posts {
+            posts {
+              _id
+              title
+              content
+              creator {
+                name
+              }
+              createdAt
+            }
+         totalPosts
+        }}`
+    }
+
+    fetch('http://localhost:8080/graphql',{
+        method: 'POST',
         headers: {
-            Authorization: 'Bearer ' + this.props.token
-        }
+            Authorization: 'Bearer ' + this.props.token,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(grapqlQuery)
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+          throw new Error('Fetching posts failed!');
+        }
         this.setState({
-          posts: resData.posts.map(post => {
+          posts: resData.data.posts.posts.map(post => {
             return {
                 ...post,
                 imagePath: post.imageUrl
             }
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.posts.totalPosts,
           postsLoading: false
         });
       })
@@ -171,19 +190,30 @@ class Feed extends Component {
               throw new Error('User LOGIN failed!');
           }
         const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt
+          _id: resData.data.createPost._id,
+          title: resData.data.createPost.title,
+          content: resData.data.createPost.content,
+          creator: resData.data.createPost.creator,
+          createdAt: resData.data.createPost.createdAt
         };
-        this.setState(prevState => {
-          return {
-            isEditing: false,
-            editPost: null,
-            editLoading: false
-          };
-        });
+          this.setState(prevState => {
+              let updatedPosts = [...prevState.posts];
+              if (prevState.editPost) {
+                  const postIndex = prevState.posts.findIndex(
+                      p => p._id === prevState.editPost._id
+                  );
+                  updatedPosts[postIndex] = post;
+              } else {
+                  updatedPosts.unshift(post);
+              }
+              return {
+                  posts: updatedPosts,
+                  isEditing: false,
+                  editPost: null,
+                  editLoading: false
+              };
+          });
+
       })
       .catch(err => {
         console.log(err);
