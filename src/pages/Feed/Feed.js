e7d10bf226a843172710e395cee52321dd1ef224
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-07-13 11:34:57 +0200

    updating and deleting posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -178,6 +178,26 @@ class Feed extends Component {
                     }
                 }
               `
+            };
+
+            if (this.state.editPost) {
+                graphqlQuery = {
+                    query: `
+                       mutation {
+                            updatePost(id: "${this.state.editPost._id}",postInput: 
+                            {title: "${postData.title}", content: "${postData.content}", imageUrl: "${imageUrl}"}) {
+                              _id
+                              title
+                              content
+                              imageUrl
+                              creator {
+                                name
+                              }
+                              createdAt
+                            }
+                       }
+                    `
+                }
             }
 
             return fetch('http://localhost:8080/graphql', {
@@ -200,13 +220,17 @@ class Feed extends Component {
           if (resData.errors) {
               throw new Error('User LOGIN failed!');
           }
+          let redDataField = 'createPost';
+          if (this.state.editPost) {
+              redDataField = 'updatePost';
+          }
         const post = {
-          _id: resData.data.createPost._id,
-          title: resData.data.createPost.title,
-          content: resData.data.createPost.content,
-          creator: resData.data.createPost.creator,
-          createdAt: resData.data.createPost.createdAt,
-          imagePath: resData.data.createPost.imageUrl
+          _id: resData.data[redDataField]._id,
+          title: resData.data[redDataField].title,
+          content: resData.data[redDataField].content,
+          creator: resData.data[redDataField].creator,
+          createdAt: resData.data[redDataField].createdAt,
+          imagePath: resData.data[redDataField].imageUrl
         };
           this.setState(prevState => {
               let updatedPosts = [...prevState.posts];
@@ -216,7 +240,9 @@ class Feed extends Component {
                   );
                   updatedPosts[postIndex] = post;
               } else {
-                  updatedPosts.pop();
+                  if (prevState.posts.length >= 2) {
+                      updatedPosts.pop();
+                  }
                   updatedPosts.unshift(post);
               }
               return {
@@ -245,19 +271,28 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE',
+    const graphqlQuery = {
+        query: `
+            mutation {
+                deletePost(id: "${postId}")
+            }
+        `
+    };
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
-          Authorization: 'Bearer ' + this.props.token
-      }
+          Authorization: 'Bearer ' + this.props.token,
+          'Content-Type': 'application/json'
+      },
+        body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Deleting a post failed!');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+            throw new Error('Deleting the post failed!');
+        }
         console.log(resData);
         this.loadPosts();
         // this.setState(prevState => {
